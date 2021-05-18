26bd00d5b065df73ffc07ed731a24033006d03b2
abhishekchandra2522k <1ac23456789@gmail.com>
2021-05-18 22:52:43 +0530

    Add dark-theme, scrollup, portfolio css type swiper

--- version2/assets/js/main.js
+++ version2/assets/js/main.js
@@ -60,7 +60,7 @@ tabs.forEach(tab => {
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('qualification__active')
         })
-        target.classList.add('qualification__Active')
+        target.classList.add('qualification__active')
 
         tabs.forEach(tab => {
             tab.classList.remove('qualification__active')
@@ -68,4 +68,102 @@ tabs.forEach(tab => {
 
         tab.classList.add('qualification__active')
     })
+})
+
+// Id remove from url
+//Get all the hyperlink elements
+var links = document.getElementsByTagName("a");
+
+//Browse the previously created array
+Array.prototype.forEach.call(links, function(elem, index) {
+  //Get the hyperlink target and if it refers to an id go inside condition
+  var elemAttr = elem.getAttribute("href");
+  if(elemAttr && elemAttr.includes("#")) {
+    //Replace the regular action with a scrolling to target on click
+    elem.addEventListener("click", function(ev) {
+      ev.preventDefault();
+      //Scroll to the target element using replace() and regex to find the href's target id
+      document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "nearest"
+          });
+    });
+  }
+});
+
+// Swiper JS
+let swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+// Scroll and Active
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+// Change bg header
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+// Show scroll top
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+
+// Dark Light Theme
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-item')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon == 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', ()=>{
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme)
+    localStorage.getItem('selected-icon', getCurrentIcon)
 })
